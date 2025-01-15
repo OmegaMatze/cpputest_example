a5b24e10e77e2f0bb710c6ca52fab20dbb590d29
Matthias Martin <matthias@martin-tue.de>
2025-01-15 10:33:11 +0100

    this now should really let it fail

--- src/pythagorean.c
+++ src/pythagorean.c
@@ -4,6 +4,6 @@
 
 float Pythagorean_hypotenuse(float x, float y)
 {
-    float hypotenuse = SquareRoot_sqrt(x*x + y*y);
+    float hypotenuse = SquareRoot_sqrt(x*x + y*y)+1;
     return hypotenuse;
 }
--- src/square_root.c
+++ src/square_root.c
@@ -4,5 +4,5 @@
 
 float SquareRoot_sqrt(float number)
 {
-    return (float)sqrt(number)+1;
+    return (float)sqrt(number);
 }
